653095df42dfdc03d657dfc28385ebfc708d6619
sehermahmud <seher.mahmud@gmail.com>
2020-08-25 15:27:44 +0600

    Added the posts

---
+++ posts/index.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+let Post = require("./models/post");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/posts', (req, res) => {
+	Post.find()
+		.then(post => res.json(post))
+		.catch(err => res.status(400).json('Error: ' + err));
+});
+
+app.post("/posts", (req, res) => {
+  const { title } = req.body;
+
+  const newPost = new Post({
+    title,
+  });
+
+  newPost
+    .save()
+    .then(() => res.json(newPost))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+const PORT = 5000;
+
+mongoose
+  .connect(
+    `mongodb+srv://seher:seher123@cluster0.si6tr.gcp.mongodb.net/miniblog?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+  )
+  .then(() => {
+    app.listen(PORT || 5000);
+    console.log(`MongoDB has been connected and listening to Port ${PORT}`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
